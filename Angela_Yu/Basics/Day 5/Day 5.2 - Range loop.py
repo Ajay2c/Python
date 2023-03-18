71400c3ea7e68b3c0cd6984cd6f5bdf61db573a6
Ajay2c <ajaychandru20@gmail.com>
2023-03-18 18:25:31 +0530

    only the project file will be there

--- Angela_Yu/Basics/Day 5/Day 5.2 - Range loop.py
+++
@@ -1,6 +0,0 @@
-
-calculate = 0
-
-for numbers in range(2, 101, 2):
-    calculate += numbers
-print(calculate)
--- Angela_Yu/Basics/Day 5/Day 5.5 -Forloop.py
+++
@@ -1,5 +0,0 @@
-fruits = ["Apple", "Pinapple", "Bananas"]
-for fruit in fruits:
-    print(fruit)
-    print(fruit + " Pie")
-    print(fruits)
