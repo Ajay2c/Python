e783b4da84b0de540086ee9338d43f24e4b57ba9
Ajay2c <ajaychandru20@gmail.com>
2023-03-23 23:21:02 +0530

    hangman updated file

--- Angela_Yu/Projects/handman.py
+++ Angela_Yu/Projects/handman.py
@@ -72,17 +72,29 @@ for letter_length in range(choosen_word_len):
     space_contain_words += "_"
     print(letter_length)
 
- 
+lives = 7 
 
-while True:
+while not lives == 0:
     if "_" in space_contain_words:    
         print("Hi welcome to the hangman challenge!!!!")
         user_guess = input(f"please enter the letter to check what word you going to find:").lower()
 
+        if user_guess not in choosen_word:
+                    lives -= 1
+                    print(stages[lives])
+                    print(f"You made mistake, now you have {lives} lives only")
+
+
         for check_positon in range(choosen_word_len):
             if user_guess == choosen_word[check_positon]:
                 space_contain_words[check_positon] = user_guess        
+            
         print(space_contain_words)      
-    else:
-        print(f"You won, the work is {choosen_word}")
-        break        
+    
+    if lives == 0:
+                print("You lost all you lifes")
+    if "_" not in space_contain_words:
+          lives = False
+          print("You won")
+
+
