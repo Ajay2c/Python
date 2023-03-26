4d2e3fd6d438a4236fcb6075234b69281da7b195
Ajay2c <ajaychandru20@gmail.com>
2023-03-26 22:31:23 +0530

    using multi-function for days in year

--- Angela_Yu/Order_of_program/testfile.py
+++ Angela_Yu/Order_of_program/testfile.py
@@ -1,42 +1,7 @@
-from replit import clear
 
-logo = '''
-                         ___________
-                         \         /
-                          )_______(
-                          |"""""""|_.-._,.---------.,_.-._
-                          |       | | |               | | ''-.
-                          |       |_| |_             _| |_..-'
-                          |_______| '-' `'---------'` '-'
-                          )"""""""(
-                         /_________\\
-                       .-------------.
-                      /_______________\\
-'''
-print(logo)
+def format_name(name,initial):
+    a=name.title()
+    b=initial.title()
+    return print(a,b)
 
-bids = {}
-
-def the_final(bids):
-    win_bid_amount = 0
-    win_bid_name = ""
-    for key in bids:
-            the_high_bid = bids[key]
-            if the_high_bid > win_bid_amount:
-                win_bid_amount = the_high_bid
-                win_bid_name += key
-                print(f"the winner is {win_bid_name}, and the bid is {win_bid_amount}")
-
-
-start_the_bid = False
-
-while not start_the_bid:
-    name = input("What is your name?: ")
-    bid = input("what is your bid?: $")
-    bids[name] = bid
-    other_bid = input('Are they any other bid? Type "yes" or "no" ')
-
-    if other_bid == "yes":
-        clear()
-    elif other_bid == "no":
-        the_final()    
+format_name("ajay","chandru")
