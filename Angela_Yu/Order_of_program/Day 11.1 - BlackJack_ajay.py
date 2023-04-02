3c0d760a5276710a3aae18ec08e0df24e343aa5e
Ajay2c <ajaychandru20@gmail.com>
2023-04-02 18:44:09 +0530

    ajay completed the blackjack game

--- Angela_Yu/Order_of_program/Day 11.1 - BlackJack_ajay.py
+++ Angela_Yu/Order_of_program/Day 11.1 - BlackJack_ajay.py
@@ -26,30 +26,81 @@ def addcards():
         user_card.append(gen_card())
         comp_card.append(gen_card())
     return
+
 def score():
     global user_score
     global comp_score
     user_score = sum(user_card)
-    comp_score = sum(comp_card)    
-    
+    comp_score = sum(comp_card)
 
-    
-    
-    if user_score == 21 and len(user_score) == 2:
+    if user_score == 21 and len(user_card) == 2 or comp_score == 21 and len(comp_card) == 2:
         return 0
     if 11 in user_card and user_score > 21:
         user_card.remove(11)
         user_card.append(1)
-        return
+        return sum(user_card)
+    elif 11 in comp_card and comp_score > 21:
+        comp_card.remove(11)
+        comp_card.append(1)
+        return sum(comp_card)
     
+def compare(user_score,comp_score):
+    if user_score > 21 and comp_score > 21:
+        return "You went over. You lose 😤"
+    if user_score == comp_score:
+        return "Draw 🙃"
+    elif comp_score == 0:
+        return "Lose, opponent has Blackjack 😱"
+    elif user_score == 0:
+        return "Win with a Blackjack 😎"
+    elif user_score > 21:
+        return "You went over. You lose 😭"
+    elif comp_score > 21:
+        return "Opponent went over. You win 😁"
+    elif user_score > comp_score:
+        return "You win 😃"
+    else:
+        return "You lose 😤"
 
+def start_game():
+    global user_card
+    global comp_card
+    global user_score
+    global comp_score
 
+    user_card = []
+    comp_card = []
+    user_score = 0
+    comp_score = 0
 
+    addcards()
+    score()
 
+    print(f"   Your cards: {user_card}, current score: {user_score}")
+    print(f"   Computer's first card: {comp_card[0]}")
 
-gen_card()
-addcards()
-score()
+    play_game = True
 
-print(user_card,comp_card)
-print(user_score,comp_score)
+    while play_game:
+        if user_score == 0 or comp_score == 0 or user_score > 21:
+            play_game = False
+        else:
+            user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ")
+            if user_should_deal == "y":
+                user_card.append(gen_card())
+                score()
+                print(f"   Your cards: {user_card}, current score: {user_score}")
+                print(f"   Computer's first card: {comp_card[0]}")
+            else:
+                play_game = False
+
+    while comp_score < 17 and comp_score != 0:
+        comp_card.append(gen_card())
+        score()
+
+    print(f"   Your final hand: {user_card}, final score: {user_score}")
+    print(f"   Computer's final hand: {comp_card}, final score: {comp_score}")
+    print(compare(user_score, comp_score))
+
+while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
+    start_game()
