ffdbf25972ad532b2f17c8f51ae6fbb27b083c77
Ajay2c <ajaychandru20@gmail.com>
2023-04-07 23:44:22 +0530

    files cleanups

--- Angela_Yu/my_Projects/Day 14. HigherLower_game/Day 14 - HigherLowerGame.py
+++ Angela_Yu/my_Projects/Day 14. HigherLower_game/Day 14 - HigherLowerGame.py
@@ -1,15 +1,14 @@
-import sys
-sys.path.append('Angela_Yu/Order_of_program')
-
 from HigherLowerGameData import data
 from art import vs, higherlower
 
-#from replit import clear
+# from replit import clear
 import random
+
 
 def get_random_account():
     """Get data from random account"""
     return random.choice(data)
+
 
 def format_data(account):
     """Format account into printable format: name, description and country"""
@@ -18,20 +17,21 @@ def format_data(account):
     country = account["country"]
     return f"{name}, a {description}, from {country}"
 
+
 def check_answer(guess, a_followers, b_followers):
     """Checks followers against user's guess 
     and returns True if they got it right.
-    Or False if they got it wrong.""" 
+    Or False if they got it wrong."""
     if a_followers > b_followers:
         return guess == "a"
     else:
         return guess == "b"
 
+
 def game():
     print(higherlower)
     score = 0
     game_should_continue = True
-    account_a = get_random_account()
     account_b = get_random_account()
 
     while game_should_continue:
@@ -44,13 +44,13 @@ def game():
         print(f"Compare A: {format_data(account_a)}.")
         print(vs)
         print(f"Against B: {format_data(account_b)}.")
-        
+
         guess = input("Who has more followers? Type 'A' or 'B': ").lower()
         a_follower_count = account_a["follower_count"]
         b_follower_count = account_b["follower_count"]
         is_correct = check_answer(guess, a_follower_count, b_follower_count)
 
-        #clear()
+        # clear()
         print(higherlower)
         if is_correct:
             score += 1
@@ -58,5 +58,6 @@ def game():
         else:
             game_should_continue = False
             print(f"Sorry, that's wrong. Final score: {score}")
+
 
 game()
