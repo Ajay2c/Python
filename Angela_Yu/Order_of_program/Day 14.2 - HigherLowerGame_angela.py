1b1c840c480e2db90ce79ca79761108730896e52
Ajay2c <ajaychandru20@gmail.com>
2023-04-07 20:23:27 +0530

    game completed

--- Angela_Yu/Order_of_program/Day 14.2 - HigherLowerGame_angela.py
+++ Angela_Yu/Order_of_program/Day 14.2 - HigherLowerGame_angela.py
@@ -30,35 +30,41 @@ def check_answer(guess, a_count, b_count):
     
 def game():
     score = 0
-    Compare_A = generate_data()
-    a_follower_count = Compare_A["follower_count"]
-    print(a_follower_count)
-
+    
     Against_B = generate_data()
     b_follower_count = Against_B["follower_count"]
     print(b_follower_count)
     
-    
+    game_start = True
+    while game_start:    
+        Compare_A = Against_B   
+        a_follower_count = Compare_A["follower_count"]
+        print(a_follower_count)
+        
+        Against_B = generate_data()
+        b_follower_count = Against_B["follower_count"]
+        print(b_follower_count)
+        
+        while a_follower_count == b_follower_count:
+            b_follower_count = generate_data()
 
-    while a_follower_count == b_follower_count:
-        b_follower_count = generate_data()
+                    
+        print(higherlower)
+        print(f"Compare_A: {format_data(Compare_A)}")
+        print(vs)
+        print(f"Against_B: {format_data(Against_B)}")
 
-                
-    print(higherlower)
-    print(f"Compare_A: {format_data(Compare_A)}")
-    print(vs)
-    print(f"Against_B: {format_data(Against_B)}")
+        guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+        is_correct = check_answer(guess, a_follower_count, b_follower_count)
 
-    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
-    is_correct = check_answer(guess, a_follower_count, b_follower_count)
+        if is_correct:
+            score += 1
+            print(f"You're right! Current score: {score}.")
+        else:
+            game_start = False
+            print(f"Sorry, that's wrong. Final score: {score}")
 
-    if is_correct:
-        score += 1
-        print(f"You're right! Current score: {score}.")
-    else:
-        print(f"Sorry, that's wrong. Final score: {score}")
+        #compare the both
 
-    #compare the both
-
-    #def compare
+        #def compare
 game()
